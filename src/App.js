64402c0f754a0678a28e1aa85d4f7193956fdef4
Jorge G <jogoram428@gmail.com>
2019-03-29 16:55:19 -0700

    added style changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
-import ImageCard from "./components/FriendCard";
+import ImageCard from "./components/ImageCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./characters.json";
+import characters from "./characters.json";
 import Nav from "./components/Nav";
 import CardWrapper from "./components/CardWrapper";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    characters
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
@@ -26,15 +26,15 @@ class App extends Component {
         <Nav />
         <Title />
         <CardWrapper>
-          {this.state.friends.map(friend => (
+          {this.state.characters.map(character => (
             <ImageCard
-              removeFriend={this.removeFriend}
-              id={friend.id}
-              key={friend.id}
-              name={friend.name}
-              image={friend.image}
-              occupation={friend.occupation}
-              location={friend.location}
+              // removeFriend={this.removeFriend}
+              id={character.id}
+              key={character.id}
+              // name={character.name}
+              image={character.image}
+              // occupation={character.occupation}
+              // location={character.location}
             />
           ))}
         </CardWrapper>
--- src/components/CardWrapper/index.js
+++ src/components/CardWrapper/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function CardWrapper(props) {
-  return <div className="cardWrapper">{props.children}</div>;
+  return <div className="container" ><div className="cardWrapper">{props.children}</div></div>;
 }
 
 export default CardWrapper;
