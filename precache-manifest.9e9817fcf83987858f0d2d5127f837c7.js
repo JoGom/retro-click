da5fe9b4525c5d639010a98831d13b3c81744747
Jorge G <jogoram428@gmail.com>
2019-03-30 10:13:11 -0700

    Updates

---
+++ precache-manifest.9e9817fcf83987858f0d2d5127f837c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "64020f91d24d6b2db27342387826f424",
+    "url": "/retro-click/static/media/space-warp_by9j6hbzs__F0000.64020f91.png"
+  },
+  {
+    "revision": "2f78c2d71a1ece4849772e00a3ee0fb6",
+    "url": "/retro-click/static/media/space-invaders-svgrepo-com.2f78c2d7.svg"
+  },
+  {
+    "revision": "9ed7d639165e14640d18",
+    "url": "/retro-click/static/js/runtime~main.a068ae3e.js"
+  },
+  {
+    "revision": "d2faa38c5867d817cbd9",
+    "url": "/retro-click/static/js/main.1455b9f3.chunk.js"
+  },
+  {
+    "revision": "28b2623acff332ede207",
+    "url": "/retro-click/static/js/2.0e8364b2.chunk.js"
+  },
+  {
+    "revision": "d2faa38c5867d817cbd9",
+    "url": "/retro-click/static/css/main.6364f353.chunk.css"
+  },
+  {
+    "revision": "28b2623acff332ede207",
+    "url": "/retro-click/static/css/2.097a27bf.chunk.css"
+  },
+  {
+    "revision": "af9354d1e9cf842f12e0ca1bf703a178",
+    "url": "/retro-click/index.html"
+  }
+];
